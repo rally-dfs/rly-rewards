61d9f7db1c28f1851ee3c0d787123054bcc7b7e9
Jono Chang <jchang@rally.io>
2022-05-20 13:38:05 -0500

    refactor/rename some bitquery solana functions
    in preparation to reuse some of this code for bitquery's ethereum APIs

--- src/chain-data-utils/bitquery.ts
+++ src/chain-data-utils/bitquery.ts
@@ -16,6 +16,48 @@ async function _queryBitqueryGQL(queryString: string, variables?: object) {
 // wait 10 seconds between API calls (this can probably be reduced if we pay for a better plan)
 const TIMEOUT_BETWEEN_CALLS = 10000;
 
+/** Helper to handle getting all pages of results (with rate limiting built in)
+ *
+ * @param queryString graphql query as a string. Must include `limit` and `offset` as inputs
+ * @param variables variables (`limit` and `offset` will be added to this)
+ * @param dataPath function used to extract desired results array from the raw json object
+ * @returns list of parametrized objects, e.g. BitquerySolanaTransfer or BitqueryEthereumTransfer
+ */
+async function _fetchAllPagesWithQueryAndVariables<T>(
+  queryString: string,
+  variables: any,
+  dataPath: (data: any) => Array<T>
+) {
+  const pageLimit = 2500;
+  const maxOffset = pageLimit * 1000000; // infinite loop protection
+
+  let allTransfers: Array<T> = [];
+
+  let offset = 0;
+  let hasMorePages = true;
+
+  while (hasMorePages && offset < maxOffset) {
+    console.log("fetching offset", offset);
+
+    variables["limit"] = pageLimit;
+    variables["offset"] = offset;
+
+    const data = await _queryBitqueryGQL(queryString, variables);
+
+    const transfers: Array<T> = dataPath(data);
+
+    allTransfers = allTransfers.concat(transfers);
+
+    hasMorePages = transfers.length == pageLimit;
+    offset += pageLimit;
+
+    // rate limiting here in case we make too many calls
+    await new Promise((f) => setTimeout(f, TIMEOUT_BETWEEN_CALLS));
+  }
+
+  return allTransfers;
+}
+
 type BitquerySolanaTransfer = {
   amount: number;
   transferType: string; // "transfer" "mint" "burn" "self", maybe others?
@@ -33,109 +75,90 @@ type BitquerySolanaTransferAccount = {
   type: string; // i think this is always "account"?
 };
 
-// Helper for tokenAccountBalanceOnDateBitquery and tokenAccountsInfoBetweenDatesBitquery, just so we can reuse the
-// code for different filters
-// `tokenAccountOwnerFilter` is either "senderAddress: {is: $tokenAccountOwnerAddress}" or
-// "receiverAddress: {is: $tokenAccountOwnerAddress}" or null (if no filter needed)
-async function _transferAmountsWithFilter(
+/** Helper for tokenAccountBalanceOnDateBitquery and tokenAccountsInfoBetweenDatesBitquery, just so we can reuse the
+ * code for different filters
+ *
+ * @param tokenMintAddress
+ * @param startDateInclusive
+ * @param endDateExclusive
+ * @param tokenAccountOwnerFilter either "senderAddress: {is: $tokenAccountOwnerAddress}" or
+ * "receiverAddress: {is: $tokenAccountOwnerAddress}" or undefined (if no filter needed)
+ * @param tokenAccountOwnerAddress only needed if `tokenAccountOwnerFilter` is not undefined
+ */
+async function _solanaTransferAmountsWithFilter(
   tokenMintAddress: string,
   startDateInclusive: Date,
   endDateExclusive: Date,
   tokenAccountOwnerFilter?: string,
   tokenAccountOwnerAddress?: string
 ) {
-  // TODO: might be safer if we pass in a set of previous txn IDs or something too to prevent under/double counting
+  // make sure to only include the gql variable if we have a filter
+  const tokenAccountOwnerFilterString = tokenAccountOwnerFilter
+    ? tokenAccountOwnerFilter
+    : "";
+  const tokenAccountOwnerVariableString = tokenAccountOwnerFilter
+    ? "$tokenAccountOwnerAddress: String!,"
+    : "";
 
-  const pageLimit = 2500;
-  const maxOffset = pageLimit * 1000000; // infinite loop protection
+  // bitquery treats endDate as inclusive, so we need to subtract 1 millisecond from endDateExclusive
+  // (bitquery doesn't have sub-second precision anyway and seems to just drop any milliseconds passed in, so this
+  // is basically the same as subtracting 1 second, i.e. we should be calling T00:00:00Z to T23:59:59Z instead of
+  // T00:00:00Z to T00:00:00Z to avoid duplicates/undercounting
+  const endDateInclusive = new Date(endDateExclusive.valueOf() - 1);
 
-  let allTransfers: Array<BitquerySolanaTransfer> = [];
-
-  let offset = 0;
-  let hasMorePages = true;
-
-  while (hasMorePages && offset < maxOffset) {
-    console.log("fetching offset", offset);
-
-    // make sure to only include the gql variable if we have a filter
-    const tokenAccountOwnerFilterString = tokenAccountOwnerFilter
-      ? tokenAccountOwnerFilter
-      : "";
-    const tokenAccountOwnerVariableString = tokenAccountOwnerFilter
-      ? "$tokenAccountOwnerAddress: String!,"
-      : "";
-
-    // bitquery treats endDate as inclusive, so we need to subtract 1 millisecond from endDateExclusive
-    // (bitquery doesn't have sub-second precision anyway and seems to just drop any milliseconds passed in, so this
-    // is basically the same as subtracting 1 second, i.e. we should be calling T00:00:00Z to T23:59:59Z instead of
-    // T00:00:00Z to T00:00:00Z to avoid duplicates/undercounting
-    const endDateInclusive = new Date(endDateExclusive.valueOf() - 1);
-
-    const data = await _queryBitqueryGQL(
-      `query TransfersForSenderAndToken(
-            $startTime: ISO8601DateTime!, $endTime: ISO8601DateTime!, 
-            $tokenMintAddress: String!,
-            ${tokenAccountOwnerVariableString}
-            $limit: Int!, $offset: Int!) {
-          solana {
-            transfers(
-              options: {limit: $limit, offset: $offset}
-              time: {between: [$startTime, $endTime]}
-              currency: {is: $tokenMintAddress}
-              ${tokenAccountOwnerFilterString}
-            ) {
-              amount
-              transferType
-              transaction {
-                signature
-                success
-              }
-              sender {
-                address
-                mintAccount
-                type
-              }
-              receiver {
-                address
-                mintAccount
-                type
-              }
-            }
-          }
-        }  
-        `,
-      {
-        startTime: startDateInclusive.toISOString(),
-        endTime: endDateInclusive.toISOString(),
-        tokenMintAddress: tokenMintAddress,
-        tokenAccountOwnerAddress: tokenAccountOwnerAddress,
-        limit: pageLimit,
-        offset: offset,
+  const queryString = `query TransfersForSenderAndToken(
+      $startTime: ISO8601DateTime!, $endTime: ISO8601DateTime!,
+      $tokenMintAddress: String!,
+      ${tokenAccountOwnerVariableString}
+      $limit: Int!, $offset: Int!) {
+    solana {
+      transfers(
+        options: {limit: $limit, offset: $offset}
+        time: {between: [$startTime, $endTime]}
+        currency: {is: $tokenMintAddress}
+        ${tokenAccountOwnerFilterString}
+      ) {
+        amount
+        transferType
+        transaction {
+          signature
+          success
+        }
+        sender {
+          address
+          mintAccount
+          type
+        }
+        receiver {
+          address
+          mintAccount
+          type
+        }
       }
-    );
-
-    // console.log("data", data, data["solana"]["transfers"]);
-
-    const transfers: Array<BitquerySolanaTransfer> =
-      data["solana"]["transfers"];
-
-    allTransfers = allTransfers.concat(transfers);
-
-    hasMorePages = transfers.length == pageLimit;
-    offset += pageLimit;
-
-    // rate limiting here in case we make too many calls
-    await new Promise((f) => setTimeout(f, TIMEOUT_BETWEEN_CALLS));
+    }
   }
+  `;
 
-  return allTransfers;
+  const variables = {
+    startTime: startDateInclusive.toISOString(),
+    endTime: endDateInclusive.toISOString(),
+    tokenMintAddress: tokenMintAddress,
+    tokenAccountOwnerAddress: tokenAccountOwnerAddress,
+    // `limit` and `offset` will be added by _fetchAllPagesWithQueryAndVariables
+  };
+
+  return _fetchAllPagesWithQueryAndVariables<BitquerySolanaTransfer>(
+    queryString,
+    variables,
+    (data) => data["solana"]["transfers"]
+  );
 }
 
 // Queries bitquery solana.transfers for `ownerAddress` with an end date of `endDateExclusive` (any transactions
 // exactly on endDateExclusive will not be counted)
 // Since bitquery results don't contain the date information and aren't guaranteed to be sorted, this is best used in
 // conjunction with tokenAccountBalanceOnDateSolanaFm to just make sure there isn't anything missing in solana.fm
-export async function allTransfersBetweenDatesBitquery(
+export async function allSolanaTransfersBetweenDatesBitquery(
   tokenAccountAddress: string,
   tokenAccountOwnerAddress: string,
   tokenMintAddress: string,
@@ -145,7 +168,7 @@ export async function allTransfersBetweenDatesBitquery(
   const allowedTransferTypes = new Set(["transfer", "mint", "burn"]);
 
   const transfersOut: Array<BitquerySolanaTransfer> =
-    await _transferAmountsWithFilter(
+    await _solanaTransferAmountsWithFilter(
       tokenMintAddress,
       startDateInclusive,
       endDateExclusive,
@@ -164,7 +187,7 @@ export async function allTransfersBetweenDatesBitquery(
 
   // same as transfersOut but filter by `receiverAddress` instead of `senderAddress`
   const transfersIn: Array<BitquerySolanaTransfer> =
-    await _transferAmountsWithFilter(
+    await _solanaTransferAmountsWithFilter(
       tokenMintAddress,
       startDateInclusive,
       endDateExclusive,
@@ -185,7 +208,7 @@ export async function allTransfersBetweenDatesBitquery(
   return filteredTransfersOut.concat(filteredTransfersIn);
 }
 
-export type BitqueryTokenAccountInfo = {
+export type BitquerySolanaTokenAccountInfo = {
   tokenAccountAddress: string;
   ownerAccountAddress?: string;
   balanceChange: number;
@@ -195,7 +218,7 @@ export type BitqueryTokenAccountInfo = {
 
 // Queries bitquery solana.transfers for all token accounts belonging to `tokenMintAddress` with any activity between
 // `startDate` and `endDate`
-// Returns a map of {tokenAccountAddress => BitqueryTokenAccountInfo} (i.e. this would probably be used to see which
+// Returns a map of {tokenAccountAddress => BitquerySolanaTokenAccountInfo} (i.e. this would probably be used to see which
 // new accounts were created that day and for updating balance/txn count for any previous accounts against some running
 // db list)
 //
@@ -205,12 +228,12 @@ export type BitqueryTokenAccountInfo = {
 // Note this interface is slightly different than tokenAccountsInfoBetweenDatesSolanaFm. Since bitquery
 // solana.transfers doesn't have any balances, we return the change in balance between startDate and endDate rather
 // than the final balance (so this must be combined with some previous call's balance or called multiple times)
-export async function tokenAccountsInfoBetweenDatesBitquery(
+export async function solanaTokenAccountsInfoBetweenDatesBitquery(
   tokenMintAddress: string,
   startDateInclusive: Date,
   endDateExclusive: Date
 ) {
-  let results = await _transferAmountsWithFilter(
+  let results = await _solanaTransferAmountsWithFilter(
     tokenMintAddress,
     startDateInclusive,
     endDateExclusive,
@@ -218,7 +241,7 @@ export async function tokenAccountsInfoBetweenDatesBitquery(
     undefined
   );
 
-  let accountInfoMap: { [key: string]: BitqueryTokenAccountInfo } = {};
+  let accountInfoMap: { [key: string]: BitquerySolanaTokenAccountInfo } = {};
 
   results.forEach((result) => {
     // TODO: currently ignoring "mint" "burn" and "self" (think this is when the same owner transfers
@@ -268,7 +291,7 @@ export async function tokenAccountsInfoBetweenDatesBitquery(
 
 // Queries solana.transactions for the transactions in transactionHashes
 // Returns a map of {txnHash: success}
-export async function getTransactionSuccessForHashesBitquery(
+export async function getSolanaTransactionSuccessForHashesBitquery(
   transactionHashes: Array<string>
 ) {
   const pageLimit = 75; // TODO: this times out sometimes even at 100? need to figure out the right limit
