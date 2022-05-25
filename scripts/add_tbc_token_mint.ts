d230da267c76f67e337f182e57dbc521daa679b2
Jono Chang <jchang@rally.io>
2022-05-25 14:51:54 -0500

    add a display_name to tbc_token_mints and token_account_mints

--- scripts/add_tbc_token_mint.ts
+++ scripts/add_tbc_token_mint.ts
@@ -5,25 +5,28 @@ import { TBCTokenMint } from "../src/knex-types/tbc_token_mint";
 /** Inserts new row to tbc_token_mints
  *
  * arg 1 is mint_address
- * arg 2 is decimals
+ * arg 2 is display_name
+ * arg 3 is decimals
  *
- * e.g. $ npm run add-tbc-token-mint RLYv2ubRMDLcGG2UyvPmnPmkfuQTsMbg4Jtygc7dmnq 9
+ * e.g. $ npm run add-tbc-token-mint RLYv2ubRMDLcGG2UyvPmnPmkfuQTsMbg4Jtygc7dmnq sRLY 9
  */
 const main = async () => {
   const knex = getKnex();
 
   const mintAddressString = process.argv[2];
-  const decimalsString = process.argv[3];
+  const displayNameString = process.argv[3];
+  const decimalsString = process.argv[4];
 
   // validate keys and convert to byte array to store in DB
 
   console.log(
-    `Adding token mint ${mintAddressString}, decimals: ${decimalsString}`
+    `Adding token mint ${displayNameString} ${mintAddressString}, decimals: ${decimalsString}`
   );
 
   const result = await knex<TBCTokenMint>("tbc_token_mints").insert(
     {
       mint_address: new PublicKey(mintAddressString!).toBytes(),
+      display_name: displayNameString,
       decimals: parseInt(decimalsString!),
     },
     "*" // need this for postgres to return the added result
