d5cf98b959ce57def0aa9dc2cfd588d323f64a5a
Jono Chang <jchang@rally.io>
2022-06-22 10:06:36 -0500

    clean up placeholder code in /
    not needed anymore since we have /vanity_metrics

--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -2,23 +2,6 @@ import { expect } from "chai";
 import request from "supertest";
 import app from "../src/app";
 
-describe("GET /", () => {
-  it("returns a payload containing all data we've collected", async () => {
-    const response = await request(app).get("/");
-
-    expect(response.status).to.equal(200);
-    expect(response.body).to.eql({
-      message: "RLY Rewards!",
-      liquidity_pools: [],
-      liquidity_balances_by_account: {},
-      tracked_tokens: [],
-      new_token_holder_dates_by_mint: {},
-      non_zero_balances_by_mint: {},
-      tracked_token_account_transactions_by_mint: {},
-    });
-  });
-});
-
 describe("GET /vanity_metrics", () => {
   it("returns a JSON payload of dynamically computed vanity metrics", async () => {
     const response = await request(app).get("/vanity_metrics");
