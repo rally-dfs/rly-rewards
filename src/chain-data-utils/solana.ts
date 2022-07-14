cd8ed5184ad67a2491079a3aeb27a263c027a243
Jono Chang <jchang@rally.io>
2022-07-14 16:05:51 -0500

    use genesysgo rpc instead of solana.com rpc
    it's free (for now) and supports 200 calls/s which is plenty

--- src/chain-data-utils/solana.ts
+++ src/chain-data-utils/solana.ts
@@ -1,11 +1,6 @@
-import {
-  clusterApiUrl,
-  Connection,
-  TransactionResponse,
-} from "@solana/web3.js";
+import { Connection } from "@solana/web3.js";
 
-const SOL_NETWORK = "mainnet-beta";
-const endpoint = clusterApiUrl(SOL_NETWORK);
+const endpoint = "https://ssc-dao.genesysgo.net/";
 const connection = new Connection(endpoint, "finalized");
 
 // just exported for test mocking, shouldn't be needed in any real code
@@ -17,8 +12,7 @@ export async function getSolanaTransaction(hash: string) {
   });
 }
 
-/** Calls solana `getTransaction` in a loop and returns the desired accounts' postBalances. `getTransactions` doesn't
- * seem to work on solana RPC due to rate limits but if we switch to a real RPC provider we can probably simplify this
+/** Calls solana `getTransaction` in a loop and returns the desired accounts' postBalances.
  *
  * @param txnInfos dictionary of {txn_hash: [list of tokenAccountOwnerAddresses to fetch balances for]}
  * @param tokenMintAddress token mint address. Calling this is only supported for one mint at a time
@@ -33,67 +27,64 @@ export async function getMultipleSolanaTransactionBalances(
   const results: { [key: string]: { [key: string]: number } } = {};
   const retries: { [key: string]: string[] } = {};
 
+  const chunkSize = 200;
+
   const txnHashes = Object.keys(txnInfos);
 
-  for (let i = 0; i < txnHashes.length; i++) {
-    // TODO: this is just using solana.com's RPC limits, can adjust a real rate limit later
-    if (i != 0 && i % 40 == 0) {
-      await new Promise((f) => setTimeout(f, 13000));
-    }
+  for (let i = 0; i < txnHashes.length; i += chunkSize) {
+    const hashesChunk = txnHashes.slice(i, i + chunkSize);
+    const transactions = await connection.getTransactions(
+      hashesChunk,
+      "confirmed"
+    );
 
-    const hash = txnHashes[i]!;
+    transactions.forEach((txnInfo, index) => {
+      const hash = hashesChunk[index]!;
 
-    let txnInfo: TransactionResponse;
-    try {
-      // batching these with Promises.all also causes rate limit overflows so just do it one at a time
-      const response = await connection.getTransaction(hash, {
-        commitment: "confirmed",
+      if (txnInfo == null) {
+        retries[hash] = txnInfos[hash]!;
+        return;
+      }
+
+      results[hash] = {};
+
+      const ownerAddresses = txnInfos[hash]!;
+      ownerAddresses.forEach((tokenAccountOwnerAddress) => {
+        const balances = txnInfo?.meta?.postTokenBalances?.filter(
+          (tokenInfo) =>
+            (tokenInfo.owner === tokenAccountOwnerAddress &&
+              tokenInfo.mint === tokenMintAddress) ||
+            // sometimes bitquery incorrectly returns the token address as the owner instead (think this happens if the
+            // account is closed and rent removed, which messes with their scraping), so we can fall back on
+            // using it as the account address instead and look it up in accountKeys
+            tokenInfo.accountIndex ==
+              txnInfo.transaction.message.accountKeys
+                .map((accountKey) => accountKey.toString())
+                .indexOf(tokenAccountOwnerAddress)
+        );
+
+        if (!balances || balances.length === 0) {
+          console.error(
+            `Couldn't find on chain balance for ${hash} and ${tokenAccountOwnerAddress}`
+          );
+          return undefined;
+        }
+
+        if (balances.length > 1) {
+          // this error is nonfatal (shouldn't really happen anyway unless there's a solana error)
+          console.error(
+            `Found more than 1 token account for txn ${hash} and ${tokenAccountOwnerAddress}`
+          );
+        }
+
+        results[hash]![tokenAccountOwnerAddress] = parseInt(
+          balances[0]!.uiTokenAmount.amount
+        );
       });
-      if (!response) {
-        throw Error("Empty txnInfo returned");
-      }
-
-      txnInfo = response;
-    } catch (error) {
-      retries[hash] = txnInfos[hash]!;
-      continue;
-    }
-
-    results[hash] = {};
-
-    const ownerAddresses = txnInfos[hash]!;
-    ownerAddresses.forEach((tokenAccountOwnerAddress) => {
-      const balances = txnInfo?.meta?.postTokenBalances?.filter(
-        (tokenInfo) =>
-          (tokenInfo.owner === tokenAccountOwnerAddress &&
-            tokenInfo.mint === tokenMintAddress) ||
-          // sometimes bitquery incorrectly returns the token address as the owner instead (think this happens if the
-          // account is closed and rent removed, which messes with their scraping), so we can fall back on
-          // using it as the account address instead and look it up in accountKeys
-          tokenInfo.accountIndex ==
-            txnInfo.transaction.message.accountKeys
-              .map((accountKey) => accountKey.toString())
-              .indexOf(tokenAccountOwnerAddress)
-      );
-
-      if (!balances || balances.length === 0) {
-        console.error(
-          `Couldn't find on chain balance for ${hash} and ${tokenAccountOwnerAddress}`
-        );
-        return undefined;
-      }
-
-      if (balances.length > 1) {
-        // this error is nonfatal (shouldn't really happen anyway unless there's a solana error)
-        console.error(
-          `Found more than 1 token account for txn ${hash} and ${tokenAccountOwnerAddress}`
-        );
-      }
-
-      results[hash]![tokenAccountOwnerAddress] = parseInt(
-        balances[0]!.uiTokenAmount.amount
-      );
     });
+
+    // rate limit is 200 RPC/second, pause 1 second after each chunk
+    await new Promise((f) => setTimeout(f, 1000));
   }
 
   // if retries are on, recursively call and merge results
